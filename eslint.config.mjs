c2f393bfd4b21edececb8760804e8b17883b33bb
carloswps <carloswps>
2026-06-17 10:33:42 -0300

    Fix all ci windows format file config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,28 +5,28 @@ import eslintPluginPrettier from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-	{
-		ignores: ['dist', 'node_modules'],
-	},
-	js.configs.recommended,
-	...tseslint.configs.recommended,
-	eslintConfigPrettier,
-	{
-		files: ['**/*.ts'],
-		languageOptions: {
-			parser: tseslint.parser,
-			parserOptions: {
-				project: true,
-			},
-			globals: {
-				...globals.node,
-			},
-		},
-		plugins: {
-			prettier: eslintPluginPrettier,
-		},
-		rules: {
-			'prettier/prettier': 'error',
-		},
-	},
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
 ];
